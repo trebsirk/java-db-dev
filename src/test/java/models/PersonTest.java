a4eba7176b435bc757383edc68e9322a2456d345
trebsirk <trebsirk@gmail.com>
2024-06-24 00:19:16 -0400

    added tests for toJSON and fromJSON

--- src/test/java/models/PersonTest.java
+++ src/test/java/models/PersonTest.java
@@ -2,8 +2,14 @@
 package models;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Optional;
 
 import org.junit.Test;
+
+import DAOs.PersonDAOImpl;
 
 public class PersonTest {
 
@@ -12,5 +18,33 @@ public class PersonTest {
         Person person = new Person(null, "John Doe", 30);
         assertEquals("John Doe", person.name());
         assertEquals(Integer.valueOf(30), person.age());
+    }
+
+    @Test
+    public void testPersonFromJSON() {
+        String jsonString = """
+        {
+            "id": 1,
+            "name": "Cleetus",
+            "age": 30
+        }
+        """;
+        Optional<Person> pOpt = new PersonDAOImpl(null).fromJSON(jsonString);
+        assertFalse(pOpt.isEmpty());
+        Person p = pOpt.get();//.orElseGet(() -> new Person(null, null, null));
+        assertEquals((int)p.id(), 1);
+        assertEquals("Cleetus", p.name());
+        assertEquals((int)p.age(), 30);
+    }
+
+    @Test
+    public void testPersonToJSON() {
+        String jsonStringExpected = """
+        {"id":1,"name":"Cleetus","age":30}
+        """;
+        Person p = new Person(1, "Cleetus", 30);
+        String jsonString = new PersonDAOImpl(null).toJSON(p);
+        assertTrue(jsonStringExpected.trim().equals(jsonString.trim()));
+                
     }
 }
