3ad858f5d7f5bc77d36e4efdac8726661771c3a6
trebsirk <trebsirk@gmail.com>
2024-06-25 16:13:10 -0400

    added UserSessionService and UserSessionDAOImpl

---
+++ src/main/java/DAOs/DataAccessException.java
@@ -0,0 +1,11 @@
+package DAOs;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+public class DataAccessException extends Throwable {
+
+    public DataAccessException(String string, JsonProcessingException e) {
+        //TODO Auto-generated constructor stub
+    }
+
+}
